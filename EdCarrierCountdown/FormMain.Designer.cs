2db2b4e0dcfe9e8d81a8cbae5452482243a2cff5
rexthecapt <rexthecapt@gmail.com>
2023-01-04 21:16:37 +0100

    update

--- EdCarrierCountdown/FormMain.Designer.cs
+++ EdCarrierCountdown/FormMain.Designer.cs
@@ -28,26 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.timerCountdown = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(249, 84);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "00:00 | 00:00 | 00:00 | 00:00\r\nSystem Name\r\n1000t | 500ly";
             // 
-            // Form1
+            // timerCountdown
+            // 
+            this.timerCountdown.Enabled = true;
+            this.timerCountdown.Tick += new System.EventHandler(this.timerCountdown_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 105);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(306, 105);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 84);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "00:00 | 00:00 | 00:00 | 00:00\r\nSystem Name\r\n1000t | 500ly";
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(306, 105);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMain";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +100,10 @@
         #endregion
 
         private Label label1;
+        private System.Windows.Forms.Timer timerCountdown;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label2;
     }
 }
--- EdCarrierCountdown/FormMain.cs
+++ EdCarrierCountdown/FormMain.cs
@@ -1,10 +1,147 @@
+using System.ComponentModel;
+
+using EdTools;
+
 namespace EdCarrierCountdown
 {
     public partial class FormMain : Form
     {
+        private JournalScanner js = new();
+        private int fuelLevel = 0;
+        private float jumpRangeCurr = 0;
+        private float jumpRangeMax = 0;
+        private string SystemName = "";
+        private DateTime jumpETA = DateTime.MinValue;
+        private List<string> lastEvents = new();
+
         public FormMain()
         {
             InitializeComponent();
+
+            JournalScanner.OnEventHandler += JournalScanner_OnEventHandler;
+
+            this.TopMost = true;
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.Parent = this;
+
+            this.DoubleBuffered = true;
+
+            label1.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+
+            label1.Parent = panel1;
+            label2.Parent = panel2;
+        }
+
+        private void JournalScanner_OnEventHandler(object? sender, EventArgs e)
+        {
+            List<string> subs = new() { "CarrierJumpRequest", "CarrierJump", "CarrierStats", "CarrierJumpCancelled", "CarrierDepositFuel" };
+            var eargs = (JournalScanner.OnEventArgs)e;
+            JsonClass.Root root = eargs.OnEvent;
+            var ev = eargs.OnEvent._event;
+
+            while (lastEvents.Count > 50)
+                lastEvents.RemoveAt(0);
+
+            DateTimeOffset dto = new(root.timestamp);
+            lastEvents.Add($"{dto.LocalDateTime} {root._event}");
+
+            //string s = "";
+
+            //foreach (string l in lastEvents)
+            //    s += $"{l}\r\n";
+
+            //textBox1.Text = s;
+
+            if (!subs.Contains(ev)) return;
+
+            switch (ev)
+            {
+                case "CarrierJumpRequest":
+                    SystemName = root.SystemName;
+                    jumpETA = dto.LocalDateTime.AddMinutes(15).AddSeconds(14);
+                    break;
+                case "CarrierJump":
+                    SystemName = root.StarSystem;
+                    break;
+                case "CarrierStats":
+                    fuelLevel = (int)root.FuelLevel;
+                    jumpRangeCurr = (int)root.JumpRangeCurr;
+                    jumpRangeMax = (int)root.JumpRangeMax;
+                    break;
+                case "CarrierJumpCancelled":
+                    break;
+                case "CarrierDepositFuel":
+                    fuelLevel = (int)root.Total;
+                    break;
+            }
+        }
+
+        int countdownTick = 0;
+        private void timerCountdown_Tick(object sender, EventArgs e)
+        {
+            countdownTick++;
+            js.Tick();
+
+            var lockdown = GetTime(jumpETA.AddMinutes(-3).AddSeconds(-20));
+            var jump = GetTime(jumpETA);
+            var EtaArrival = GetTime(jumpETA.AddSeconds(60));
+            var cooldown = GetTime(jumpETA.AddMinutes(5).AddSeconds(-10), false);
+
+            string output = $"{lockdown:mm\\:ss} | {jump:mm\\:ss} | {EtaArrival:mm\\:ss} | {cooldown:mm\\:ss}\n{SystemName}\n{fuelLevel}t | {jumpRangeCurr}ly";
+
+            label1.Text = label2.Text = output;
+
+            TimeSpan maxmin = new(0, 20, 0);
+
+            if (countdownTick % 10 != 0) return;
+
+            if (countdownTick % 20 == 0)
+                panel1.BackgroundImage?.Dispose();
+            if (countdownTick % 20 == 10)
+                panel2.BackgroundImage?.Dispose();
+            
+            var width = this.Width;
+            var img = new Bitmap(width,1);
+
+            for (int w = 0; w < width; w++)
+            {
+                double part = maxmin.TotalMilliseconds / width * w;
+
+                Color c;
+
+                if (lockdown.TotalMilliseconds > part)
+                    c = Color.Magenta;
+                else if (jump.TotalMilliseconds > part)
+                    c = Color.Green;
+                else if (EtaArrival.TotalMilliseconds > part)
+                    c = Color.Yellow;
+                else if (cooldown.TotalMilliseconds > part)
+                    c = Color.Red;
+                else
+                    c = this.BackColor;
+
+                img.SetPixel(w, 0, c);
+            }
+
+            if (countdownTick % 20 == 0)
+            {
+                panel1.BackgroundImage = img;
+                panel1.BringToFront();
+            }
+            if (countdownTick % 20 == 10)
+            {
+                panel2.BackgroundImage = img;
+                panel2.BringToFront();
+            }
+        }
+
+        private TimeSpan GetTime(DateTime time, bool stayNegative = true)
+        {
+            if (DateTime.Now > time && stayNegative)
+                time = DateTime.Now;
+            return time - DateTime.Now;
         }
     }
 }
